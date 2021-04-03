8c8d5e0559445988154a8f73a0c7119788638739
Rauha <yunlong.a.liu@gmail.com>
2021-04-03 19:06:58 +0300

    update docker_hub

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+ const FAVORITE_FOOD = process.env.NAME;
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    console.log('Microservices rock!');
+    console.log(`${FAVORITE_FOOD} rocks!`);
     await sleep(5000);
   }
 }
